273f0586d04ced4f29aca1313648437eefa8d6f6
Hasanat1992 <151622839+Hasanat1992@users.noreply.github.com>
2024-05-09 15:41:52 +0600

    first deploying commit

---
+++ JWTAngular/src/app/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: any[] = [];
+  username: string = ''
+
+  constructor
+    (private authService: AuthService,
+      private storageService: StorageService
+    ) { }
+
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.username = this.storageService.getUser().userName;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { username, password } = this.form;
+    // console.log("Role---- -- -- ", this.form)
+
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        console.log("Role---- -- -- ",data)
+        this.reloadPage();
+        this.storageService.saveUser(data);
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        const user = this.storageService.getUser();
+        // console.log("user-----",user)
+        this.username = this.storageService.getUser().userName;
+        console.log("user name-----", this.username)
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+}
